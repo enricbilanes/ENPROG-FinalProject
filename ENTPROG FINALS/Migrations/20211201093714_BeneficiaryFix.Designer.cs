b451b24f6bb0889d88d0ab8c5a8f4e3e83f5e37c
enricbilanes <89582934+enricbilanes@users.noreply.github.com>
2021-12-01 17:37:48 +0800

    Beneficiary fix

--- ENTPROG FINALS/Migrations/20211201093714_BeneficiaryFix.Designer.cs
+++ ENTPROG FINALS/Migrations/20211201093714_BeneficiaryFix.Designer.cs
@@ -4,14 +4,16 @@ using ENTPROG_FINALS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ENTPROG_FINALS.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211201093714_BeneficiaryFix")]
+    partial class BeneficiaryFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ENTPROG FINALS/Migrations/20211201093714_BeneficiaryFix.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ENTPROG_FINALS.Migrations
+{
+    public partial class BeneficiaryFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Decsription",
+                table: "Beneficiaries",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Beneficiaries",
+                newName: "Decsription");
+        }
+    }
+}
